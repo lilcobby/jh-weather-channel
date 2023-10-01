be0efba1b92e77bcf1610dbe20a8aacd6a2d3773
lilcobby <jshairfield13@gmail.com>
2023-10-01 18:31:00 -0400

    still working on passing data to 5 day

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
 // this returns the lat and long of city name in the input field
-const apiKey = "ba2dc7edbfab0d54f19c776d6eef06fe";
-const apiUrl =
-  "https://api.openweathermap.org/geo/1.0/direct?" +
-  "appid=" +
-  apiKey +
-  "&units=imperial&q=";
+// const apiKey = "ba2dc7edbfab0d54f19c776d6eef06fe";
+// const apiUrl =
+//   "https://api.openweathermap.org/geo/1.0/direct?" +
+//   "appid=ba2dc7edbfab0d54f19c776d6eef06fe" +
+//   apiKey +
+//   "&units=imperial&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+var theData;
+// let cityLat = "";
+// let cityLon = "";
 
-async function checkWeather(city) {
-  const response = await fetch(apiUrl + city);
-  var data = await response.json();
+function getApi(city) {
+  var apiUrl =
+    "https://api.openweathermap.org/geo/1.0/direct?" +
+    "appid=ba2dc7edbfab0d54f19c776d6eef06fe" +
+    "&units=imperial&q=";
 
-  console.log(data);
-
-  document.querySelector(".city").innerHTML = data.name;
+  fetch(apiUrl + city)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let theData = data;
+      //   cityLat = theData[0].lat;
+      //   cityLon = theData[0].lon;
+      console.log(theData[0].lat);
+      return theData;
+    });
 }
 
 searchBtn.addEventListener("click", () => {
-  checkWeather(searchBox.value);
+  getApi(searchBox.value);
+  console.log(theData);
 });
